d16a6f81a1ff3b83f1b2cc3b9326e7f7b86440e8
PD <dulnev.fortech@gmail.com>
2025-07-31 14:51:50 +0300

    TASK-1: init files

---
+++ telegram-bot/gate-bot/src/main/java/ru/fortech/GateBotApplication.java
@@ -0,0 +1,9 @@
+package ru.fortech;
+
+import org.springframework.boot.SpringApplication;
+
+public class GateBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateBotApplication.class, args);
+    }
+}
